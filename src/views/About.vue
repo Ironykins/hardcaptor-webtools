5acc87c53045833f3618d4d83d441b203a3cd391
Konrad Aust <konrad.aust@gmail.com>
2020-06-25 23:52:23 -0600

    Updated about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,7 +2,9 @@
   <div class="about">
     <img alt="HARDCAPTOR SAKUGA: FULL METAL PETTICOAT" class="pure-img" src="@/assets/banner.png">
     <p>
-      HARDCAPTOR SAKUGA: FULL METAL PETTICOAT is a Tabletop Roleplaying Game about magical girls. Players take on the role of an adolescent girl who finds herself thrust headfirst into conflict with cosmic, unknowable evil. She must do battle against magical manifestations of the darkest parts of humanity and unravel the mysteries of the world she thought she knew, all while keeping on top of her schoolwork and personal life.
+      HARDCAPTOR SAKUGA: FULL METAL PETTICOAT is a Tabletop Roleplaying Game about Magical Girls.
+      Players take on the role of an adolescent girl who finds herself thrust headfirst into conflict with cosmic, unknowable evil.
+      Player characters will battle against magical manifestations of the darkest parts of humanity, unravel the mysteries of the world, and struggle to keep on top of their schoolwork and personal lives.
     </p>
     <p>
       This app is a web tool for creating a character and some other fun system-related things.
